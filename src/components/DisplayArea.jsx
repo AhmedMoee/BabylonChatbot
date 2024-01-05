407b60c977722b05579e61ba1244033c924391fa
AhmedMoee <bkr8gd@virginia.edu>
2024-01-05 14:41:16 +0300

    Added text boxes for each output

--- src/components/DisplayArea.jsx
+++ src/components/DisplayArea.jsx
@@ -2,7 +2,7 @@ export const DisplayArea = ({ textArray }) => {
 	return (
 		<>
 			{textArray.map((element, index) => (
-				<p key={index}>
+				<p key={index} className="text-box">
 					{element}
 					<br />
 				</p>
