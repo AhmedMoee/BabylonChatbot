65448e03979e2c52772ae0339463847d27ad13b6
AhmedMoee <bkr8gd@virginia.edu>
2024-01-05 14:47:04 +0300

    scrollable view for chats

--- src/components/DisplayArea.jsx
+++ src/components/DisplayArea.jsx
@@ -1,12 +1,12 @@
 export const DisplayArea = ({ textArray }) => {
 	return (
-		<>
+		<div className="chat-container">
 			{textArray.map((element, index) => (
 				<p key={index} className="text-box">
 					{element}
 					<br />
 				</p>
 			))}
-		</>
+		</div>
 	);
 };
