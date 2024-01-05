904382a959dfe6264aa9ec4151fee82d1efb0a71
maggiehollis <mhollis@smith.edu>
2024-01-05 14:34:16 -0500

    Add thread and assistant initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import "./App.css";
 import { ChatInterface } from "./components/ChatInterface";
+import { useState, useEffect } from "react";
+import { getThread, createOpenAI, getAssistant } from "./utils.js";
 
 function App() {
-	return (
-		<>
-			<h1>Chatbot</h1>
+  const openai = createOpenAI();
+  const [thread_id, setThreadID] = useState("");
 
-			<ChatInterface />
-		</>
-	);
+  useEffect(() => {
+    const func = async () => {
+      const thread = await getThread(openai);
+      setThreadID(thread);
+    };
+    func();
+  }, []);
+  console.log(thread_id);
+  const assistant = getAssistant();
+
+  const run = async () => {
+    const run = await openai.beta.threads.runs.create(thread_id, {
+      assistant_id: assistant,
+    });
+    return run;
+  };
+
+  run();
+
+  return (
+    <>
+      <h1>Chatbot</h1>
+      <ChatInterface />
+    </>
+  );
 }
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,30 @@
+import OpenAI from "openai";
+
 // Contains functions you can call from Component (‘jsx’) files.
 // These functions will handle creating threads, runs, messages, and receiving the response from the assistant.
+const assistant = import.meta.env.VITE_ASSISTANT_ID;
+const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+
+export const createOpenAI = () => {
+  return new OpenAI({
+    apiKey: apiKey,
+    dangerouslyAllowBrowser: true,
+  });
+};
+
+export const getThread = async (openai) => {
+  const thread = await openai.beta.threads.create({
+    messages: [
+      {
+        role: "user",
+        content: "Say hi",
+        file_ids: [],
+      },
+    ],
+  });
+  return thread.id;
+};
+
+export const getAssistant = () => {
+  return assistant;
+};
