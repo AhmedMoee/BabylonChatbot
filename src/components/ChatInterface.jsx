f3fe48aea8fb7854bc55d703cedd0d106c172575
maggiehollis <mhollis@smith.edu>
2024-01-08 13:22:49 -0500

    Fix delayed message print

--- src/components/ChatInterface.jsx
+++ src/components/ChatInterface.jsx
@@ -3,92 +3,77 @@ import { useState, useEffect } from "react";
 import { getThread, createOpenAI, getAssistant } from "../utils.js";
 
 const cycle = async (message, thread_id, assistant, openai, setCompleted) => {
-	const addMessage = async () => {
-		await openai.beta.threads.messages.create(thread_id, {
-			role: "user",
-			content: message,
-		});
-	};
-	addMessage();
+  await openai.beta.threads.messages.create(thread_id, {
+    role: "user",
+    content: message,
+  });
+  const run = await openai.beta.threads.runs.create(thread_id, {
+    assistant_id: assistant,
+  });
 
-	const run = async () => {
-		const run = await openai.beta.threads.runs.create(thread_id, {
-			assistant_id: assistant,
-		});
-
-		let timeElapsed = 0;
-		while (timeElapsed < 100) {
-			const retreiveRun = await openai.beta.threads.runs.retrieve(
-				thread_id,
-				run.id
-			);
-			if (retreiveRun.status === "completed") {
-				setCompleted(true);
-				return;
-			}
-
-			timeElapsed += 1;
-		}
-	};
-	run();
+  let timeElapsed = 0;
+  while (timeElapsed < 100) {
+    const retreiveRun = await openai.beta.threads.runs.retrieve(
+      thread_id,
+      run.id
+    );
+    if (retreiveRun.status === "completed") {
+      printMessages(thread_id, openai);
+      return;
+    }
+    timeElapsed += 1;
+  }
+  console.log("failed to respond in time");
 };
 
-const printMessages = async (thread_id, completed, openai) => {
-	const listMessages = async () => {
-		const threadMessages = await openai.beta.threads.messages.list(thread_id);
+const printMessages = async (thread_id, openai) => {
+  const threadMessages = await openai.beta.threads.messages.list(thread_id);
 
-		console.log("-".repeat(100));
+  console.log("-".repeat(100));
 
-		for (let i = threadMessages.data.length - 1; i >= 0; i--) {
-			console.log(threadMessages.data[i].content[0].text.value);
-		}
-	};
-
-	if (completed) {
-		listMessages();
-	}
+  for (let i = threadMessages.data.length - 1; i >= 0; i--) {
+    console.log(threadMessages.data[i].content[0].text.value);
+  }
 };
 
 export const ChatInterface = () => {
-	const [text, setText] = useState("");
-	const [textArray, setTextArray] = useState([]);
+  const [text, setText] = useState("");
+  const [textArray, setTextArray] = useState([]);
 
-	const openai = createOpenAI();
-	const assistant = getAssistant();
-	const [thread_id, setThreadID] = useState("");
-	const [completed, setCompleted] = useState(false);
+  const openai = createOpenAI();
+  const assistant = getAssistant();
+  const [thread_id, setThreadID] = useState("");
 
-	useEffect(() => {
-		const func = async () => {
-			const thread = await getThread(openai);
-			setThreadID(thread);
-		};
-		func();
-	}, []);
+  useEffect(() => {
+    const func = async () => {
+      const thread = await getThread(openai);
+      setThreadID(thread);
+    };
+    func();
+  }, []);
 
-	return (
-		<>
-			<DisplayArea textArray={textArray} />
+  return (
+    <>
+      <DisplayArea textArray={textArray} />
 
-			<form
-				onSubmit={(event) => {
-					event.preventDefault();
-					setTextArray((textArray) => [...textArray, text]);
-					setText("");
-					printMessages(thread_id, completed, openai);
-					cycle(text, thread_id, assistant, openai, setCompleted);
-				}}
-			>
-				<input
-					type="text"
-					id="input"
-					placeholder="Enter a question"
-					value={text}
-					onChange={(event) => setText(event.target.value)}
-					className="input-box"
-				/>
-				<input type="submit" value="Submit" className="submit-box" />
-			</form>
-		</>
-	);
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setTextArray((textArray) => [...textArray, text]);
+          setText("");
+          cycle(text, thread_id, assistant, openai);
+        }}
+      >
+        <input
+          type="text"
+          id="input"
+          placeholder="Enter a question"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          className="input-box"
+        />
+        <input type="submit" value="Submit" className="submit-box" />
+      </form>
+    </>
+  );
 };
