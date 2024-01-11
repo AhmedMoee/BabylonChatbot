7e10aa4536ac067fbdb8a6d11047c089c102610b
nyccreator <andyfernandeznyc@gmail.com>
2024-01-11 16:37:05 -0500

    Convert the side bar to a bottom bar on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
 	useEffect(() => {
 		let handler = (e) => {
 			if (!popUpRef.current.contains(e.target)) {
-				setRatingPopup(false)
+				/* empty */
 			}
 		}
 		document.addEventListener('mousedown', handler)
@@ -31,7 +31,7 @@ export default function App() {
 		<div className={colorScheme}>
 			<div className="flex h-dvh w-dvw gap-0 bg-babylon-blue-light dark:bg-babylon-blue-dark">
 				<div
-					className="w-1/16 min-w-18 z-40 hidden content-center gap-10 bg-babylon-blue-dark p-5 pt-10 sm:flex sm:flex-col dark:bg-babylon-blue-light"
+					className="z-40 hidden w-1/16 min-w-18 content-center gap-10 bg-babylon-blue-dark p-5 pt-10 dark:bg-babylon-blue-light sm:flex sm:flex-col"
 					ref={popUpRef}
 				>
 					<button className="hover:animate-vote" onClick={() => setProfilePopup(!profilePopup)}>
@@ -67,7 +67,7 @@ export default function App() {
 					<PopUp trigger={profilePopup} onClose={() => setProfilePopup(false)}>
 						{isProfileSet ? (
 							<div className="relative flex flex-col items-center justify-center gap-10 text-2xl">
-								<div className="inline-flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-babylon-blue-dark text-white ring ring-white sm:text-base md:h-16 md:w-16 md:text-lg lg:h-16 lg:w-16 lg:text-xl dark:bg-babylon-blue-light">
+								<div className="inline-flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-babylon-blue-dark text-white ring ring-white dark:bg-babylon-blue-light sm:text-base md:h-16 md:w-16 md:text-lg lg:h-16 lg:w-16 lg:text-xl">
 									<img
 										className="inline-block h-12 w-12 rounded-full ring ring-white md:h-16 md:w-16 lg:h-16 lg:w-16"
 										src="/src/assets/user.png"
@@ -87,7 +87,7 @@ export default function App() {
 									<input
 										className="block w-full rounded-3xl border border-slate-300 bg-white py-2 pl-9 pr-3 font-medium drop-shadow-lg placeholder:italic placeholder:text-slate-400 focus:border-babylon-blue-dark focus:outline-none focus:ring-1 focus:ring-babylon-blue-dark sm:text-base"
 										type="text"
-										id="input"
+										id="username"
 										placeholder="Username"
 										value={username}
 										onChange={(event) => setUsername(event.target.value)}
@@ -96,7 +96,7 @@ export default function App() {
 									<input
 										className="block w-full rounded-3xl border border-slate-300 bg-white py-2 pl-9 pr-3 font-medium drop-shadow-lg placeholder:italic placeholder:text-slate-400 focus:border-babylon-blue-dark focus:outline-none focus:ring-1 focus:ring-babylon-blue-dark sm:text-base"
 										type="text"
-										id="input"
+										id="password"
 										placeholder="Password"
 										value={password}
 										onChange={(event) => setPassword(event.target.value)}
@@ -191,7 +191,168 @@ export default function App() {
 					style={{ backgroundImage: 'url(src/assets/Babylon_Leaf.png)' }}
 				>
 					<ChatInterface />
+
+					<div
+						className=" h-1/26 z-40 m-5 mt-4 flex flex-row content-center gap-10 sm:hidden"
+						ref={popUpRef}
+					>
+						<button
+							className=" grow-0 hover:animate-vote"
+							onClick={() => setProfilePopup(!profilePopup)}
+						>
+							<img className="" src="/src/assets/Profile-Icon.png" alt="logo" />
+						</button>
+
+						<button className="hover:animate-vote" onClick={() => setRatingPopup(!ratingPopup)}>
+							<img className="" src="/src/assets/Star-Icon.png" alt="logo" />
+						</button>
+
+						<button
+							className="hover:animate-vote"
+							onClick={() => setColorScheme(colorScheme === 'dark' ? '' : 'dark')}
+						>
+							{colorScheme === 'dark' ? (
+								<img className="" src="/src/assets/moon.png" alt="logo" />
+							) : (
+								<img className="" src="/src/assets/sun.png" alt="logo" />
+							)}
+						</button>
+
+						<button className="hover:animate-vote" onClick={() => setFaqPopup(!faqPopup)}>
+							<img className="" src="/src/assets/faq.png" alt="logo" />
+						</button>
+
+						<PopUp trigger={ratingPopup} onClose={() => setRatingPopup(false)}>
+							<h3 className="flex justify-center text-2xl font-bold text-babylon-blue-dark">
+								Rate Micro-Bot!
+							</h3>
+							<StarRating />
+						</PopUp>
+
+						<PopUp trigger={profilePopup} onClose={() => setProfilePopup(false)}>
+							{isProfileSet ? (
+								<div className="relative flex flex-col items-center justify-center gap-10 text-2xl">
+									<div className="inline-flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-babylon-blue-dark text-white ring ring-white dark:bg-babylon-blue-light sm:text-base md:h-16 md:w-16 md:text-lg lg:h-16 lg:w-16 lg:text-xl">
+										<img
+											className="inline-block h-12 w-12 rounded-full ring ring-white md:h-16 md:w-16 lg:h-16 lg:w-16"
+											src="/src/assets/user.png"
+											alt="logo"
+										/>
+									</div>
+									<p className="flex text-babylon-blue-dark">{username}</p>
+								</div>
+							) : (
+								<form
+									onSubmit={(event) => {
+										event.preventDefault()
+										setIsProfileSet(true)
+									}}
+								>
+									<div className="relative flex flex-col items-center justify-center gap-10">
+										<input
+											className="block w-full rounded-3xl border border-slate-300 bg-white py-2 pl-9 pr-3 font-medium drop-shadow-lg placeholder:italic placeholder:text-slate-400 focus:border-babylon-blue-dark focus:outline-none focus:ring-1 focus:ring-babylon-blue-dark sm:text-base"
+											type="text"
+											id="username"
+											placeholder="Username"
+											value={username}
+											onChange={(event) => setUsername(event.target.value)}
+											required
+										/>
+										<input
+											className="block w-full rounded-3xl border border-slate-300 bg-white py-2 pl-9 pr-3 font-medium drop-shadow-lg placeholder:italic placeholder:text-slate-400 focus:border-babylon-blue-dark focus:outline-none focus:ring-1 focus:ring-babylon-blue-dark sm:text-base"
+											type="text"
+											id="password"
+											placeholder="Password"
+											value={password}
+											onChange={(event) => setPassword(event.target.value)}
+											required
+										/>
+										<input
+											type="submit"
+											id="button"
+											value="Log in"
+											className=" rounded-3xl border-2 border-babylon-blue-dark bg-white px-4 py-1 text-sm font-semibold text-black hover:border-transparent hover:bg-babylon-blue-dark hover:text-white focus:outline-none focus:ring-2 focus:ring-babylon-blue-dark focus:ring-offset-2"
+										/>
+									</div>
+								</form>
+							)}
+						</PopUp>
+
+						<PopUp trigger={faqPopup} onClose={() => setFaqPopup(false)}>
+							<div className="flex flex-col justify-center gap-5 text-pretty text-babylon-blue-dark">
+								<div>
+									<p className="text-2xl font-bold">What is hydroponics?</p>
+									<p className="text-lg font-medium">
+										Hydroponics is the growing of plants without soil, whereby nutrients are
+										delivered to plants via water.
+									</p>
+								</div>
+								<div>
+									<p className="text-2xl font-bold">Why hydroponics?</p>
+									<p className="text-lg font-medium">
+										Because it is better for the planet! Hydroponically grown greens use 90% less
+										water than traditional methods, all without the use of harmful pesticides. Plus
+										the greens are better for you.
+									</p>
+								</div>
+								<div>
+									<p className="text-2xl font-bold">How do the Micro-Farms work?</p>
+									<p className="text-lg font-medium">
+										Each Micro-Farm features sophisticated software and sensors that work together
+										to ensure crops get just the right amount of light, water and nutrients. And
+										Babylon’s mobile app helps you keep tabs on your crops and get notified when
+										they’re ready for harvest.
+									</p>
+								</div>
+								<div>
+									<p className="text-2xl font-bold">How much does one cost?</p>
+									<p className="text-lg font-medium">
+										There are several different pricing options. Please contact us to learn more.
+									</p>
+								</div>
+								<div>
+									<p className="text-2xl font-bold">How much work is involved in growing?</p>
+									<p className="text-lg font-medium">
+										Our technology and mobile app make it simple to operate. Farming typically takes
+										an hour or so per week. We also have partners in select cities who can handle
+										farm operation for you, if you’re interested.
+									</p>
+								</div>
+								<div>
+									<p className="text-2xl font-bold">Why is the company called Babylon?</p>
+									<p className="text-lg font-medium">
+										Our founders Alexander and Graham drew inspiration from the proto-hydroponics of
+										the hanging gardens of Babylon, one of the original seven wonders of the ancient
+										world.
+									</p>
+								</div>
+								<div>
+									<p className="text-2xl font-bold">Do you offer a farm for personal use?</p>
+									<p className="text-lg font-medium">Not at this time.</p>
+								</div>
+								<div>
+									<p className="text-2xl font-bold">Do your farms grow marijuana?</p>
+									<p className="text-lg font-medium">
+										No. Our customers use their Micro-Farms to grow leafy greens, herbs, edible
+										flowers, and microgreens.
+									</p>
+								</div>
+								<div>
+									<p className="text-2xl font-bold">Do you ship outside the US?</p>
+									<p className="text-lg font-medium">
+										Yes. We currently have a number of clients outside the US.
+									</p>
+								</div>
+								<div>
+									<p className="text-2xl font-bold">Are you hiring?</p>
+									<p className="text-lg font-medium">Please visit our careers page.</p>
+								</div>
+							</div>
+						</PopUp>
+					</div>
 				</div>
+
+				{/* Horizontal bar for mobile */}
 			</div>
 		</div>
 	)
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -5,8 +5,8 @@ export const PopUp = (props) => {
 		<div className="fixed inset-0 bg-transparent">
 			<div className="absolute inset-0" onClick={props.onClose} />
 			<div
-				className="fixed overflow-y-auto rounded-3xl border-2 border-gray-200 bg-babylon-green p-5 shadow-lg md:p-10 lg:p-16 dark:to-babylon-blue-light"
-				style={{ width: '80%', height: '40%', maxWidth: '30rem', left: '35%', top: '25%' }}
+				className="fixed left-7/20 top-1/4 h-2/5 w-2/6 overflow-y-auto rounded-3xl border-2 border-gray-200 bg-babylon-green p-5 shadow-lg dark:to-babylon-blue-light md:p-10 lg:p-16"
+				// style={{ width: '80%', height: '40%', maxWidth: '30rem', left: '35%', top: '25%' }}
 			>
 				{props.children}
 			</div>
