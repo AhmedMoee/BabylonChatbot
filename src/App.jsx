bc500338c35e6b226ebd5683ecb9c347c5544d7f
AhmedMoee <ahmedm@virginia.edu>
2024-01-11 18:31:50 +0300

    star animation when hovering and fixing mobile view of FAQ

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect, useRef } from 'react'
 
 export default function App() {
 	const [buttonPopup, setButtonPopup] = useState(false);
+	const [hoverStar, setHoverStar] = useState(false);
 
 	let popUpRef = useRef();
 
@@ -49,17 +50,21 @@ export default function App() {
 				<ChatInterface />
 
 				<div ref={popUpRef} className="h-18 top-7/8 fixed w-dvw bg-babylon-blue-dark p-5 sm:left-0 sm:top-0 sm:h-dvh sm:w-auto md:left-0 md:top-0 md:h-dvh md:w-auto lg:left-0 lg:top-0 lg:h-dvh lg:w-auto">
-					<div className="flex flex-col items-center ">
-						<button onClick={() => setButtonPopup(!buttonPopup)}>
+					<div className="flex flex-row items-start md:flex-col md:items-center lg:flex-col lg:items-center">
+						<button 
+							onMouseEnter={() => setHoverStar(true)}
+							onMouseLeave={() => setHoverStar(false)}
+							onClick={() => setButtonPopup(!buttonPopup)}
+						>
 							<img
 								className="h-8 w-8 md:h-12 md:w-12 lg:h-12 lg:w-12"
-								src="/src/assets/star.png"
+								src={hoverStar ? "/src/assets/filled-star.png" : "/src/assets/star.png"}
 								alt="logo"
 							/>
 						</button>
 
 						<a 
-							className='inline-block mt-2 text-white ring-2 ring-white rounded px-4 py-2 hover:bg-white hover:text-babylon-blue-dark transition-colors'
+							className='mt-0 ml-7 md:mt-8 md:ml-0 lg:mt-8 lg:ml-0 text-white ring-2 ring-white rounded px-4 py-2 hover:bg-white hover:text-babylon-blue-dark transition-colors'
 							href="https://babylonmicrofarms.com/faq/" 
 							target="_blank"
 						>
